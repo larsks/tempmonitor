381b6e5690f94c1196e71162b7cb06974c0cc18b
Lars Kellogg-Stedman <lars@redhat.com>
2017-11-09 14:55:12 -0500

    added tox framework

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tempmonitor',
+    version='0.1',
+    author='Lars Kellogg-Stedman',
+    author_email='lars@oddbit.com',
+    description='micropython based wireless temperature/humidity monitoring',
+    license='GPLv3',
+    url='https://github.com/larsks/tempmonitor',
+    packages=find_packages(),
+)
