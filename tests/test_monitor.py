594a3d646b7a63ba582506f064cc85f967d64868
Lars Kellogg-Stedman <lars@redhat.com>
2017-11-09 21:37:13 -0500

    correct errors that cropped up running under micropython

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -15,8 +15,8 @@ class TestSleep(TestCase):
         self.mock_Pin = MagicMock()
         self.mock_machine.Pin.return_value = self.mock_Pin
 
-        self.mock_DHT = MagicMock()
-        self.mock_dht.DHT.return_value = self.mock_DHT
+        self.mock_DHT22 = MagicMock()
+        self.mock_dht.DHT22.return_value = self.mock_DHT22
 
         self.modules = {
             'machine': self.mock_machine,
@@ -43,7 +43,7 @@ class TestSleep(TestCase):
 
             self.mock_machine.Pin.assert_called_with(
                 config['dht_pin'])
-            assert self.mock_dht.DHT.called
+            assert self.mock_dht.DHT22.called
 
     def test_sample(self):
         with self.module_patcher:
@@ -52,8 +52,8 @@ class TestSleep(TestCase):
             expected_tmp = 10
             expected_hum = 20
 
-            self.mock_DHT.temperature.return_value = expected_tmp
-            self.mock_DHT.humidity.return_value = expected_hum
+            self.mock_DHT22.temperature.return_value = expected_tmp
+            self.mock_DHT22.humidity.return_value = expected_hum
 
             config = {
                 'dht_pin': 1,
@@ -62,6 +62,6 @@ class TestSleep(TestCase):
             m = monitor.Monitor(config)
             s = next(m.sample())
 
-            assert self.mock_DHT.measure.called
+            assert self.mock_DHT22.measure.called
             assert s['humidity'] == expected_hum
             assert s['temperature'] == expected_tmp
