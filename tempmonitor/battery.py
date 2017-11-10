422494fa021ca059349af364efc0e75ccae5834c
Lars Kellogg-Stedman <lars@redhat.com>
2017-11-09 21:13:36 -0500

    pep8 fixes

--- tempmonitor/battery.py
+++ tempmonitor/battery.py
@@ -7,13 +7,15 @@ import machine
 ADC_MODE_VCC = 255
 ADC_MODE_ADC = 0
 
+
 def set_adc_mode(mode):
     sector_size = bdev.SEC_SIZE
-    flash_size = esp.flash_size() # device dependent
+    flash_size = esp.flash_size()
     init_sector = int(flash_size / sector_size - 4)
     data = bytearray(esp.flash_read(init_sector * sector_size, sector_size))
     if data[107] == mode:
-        return  # flash is already correct; nothing to do
+        # flash is already correct; nothing to do
+        return
     else:
         data[107] = mode  # re-write flash
         esp.flash_erase(init_sector)
@@ -21,11 +23,14 @@ def set_adc_mode(mode):
         print("ADC mode changed in flash; restart to use it!")
         return
 
+
 def enable_battery_monitor():
     set_adc_mode(ADC_MODE_VCC)
 
+
 def disable_battery_monitor():
     set_adc_mode(ADC_MODE_ADC)
+
 
 def battery():
     vcc = machine.ADC(1)
