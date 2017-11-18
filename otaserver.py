92b10be30de1b9226d536cb392ccce6f8b3a712a
Lars Kellogg-Stedman <lars@oddbit.com>
2017-11-17 21:21:06 -0500

    mostly replaced with fileops implementation from noggin

--- otaserver.py
+++ otaserver.py
@@ -1,81 +1,137 @@
-import board
 import errno
+import gc
+import machine
 import os
 
-from noggin.app import App, HTTPError
+from noggin import Noggin, Response, HTTPError
+from noggin.util import chunked_reader
 
-app = App()
+import board
+
+# cribbed from
+# https://github.com/micropython/micropython-lib/blob/master/stat/stat.py
+S_IFDIR = 0o040000
+S_IFMT = 0o170000
+
+app = Noggin()
 
 
 @app.route('/')
-def index(req, match):
+def index(req):
     return {
-        'sensor_id': board.id(),
         'sensor_type': 'dht',
+        'sensor_id': board.id()
     }
 
 
-@app.route('/files/(.*)')
-def get_file(req, match):
-    path = match.group(1)
+def get_statvfs():
+    statvfs_fields = [
+        'bsize',
+        'frsize',
+        'blocks',
+        'bfree',
+        'bavail',
+        'files',
+        'ffree',
+    ]
+    return dict(zip(statvfs_fields, os.statvfs('/')))
+
+
+@app.route('/disk/free')
+def disk_free(req):
+    s = get_statvfs()
+    return {
+        'blocks': s['bfree'],
+        'bytes': (s['bsize'] * s['bfree'])
+    }
+
+
+@app.route('/mem/free')
+def mem_free(req):
+    return {
+        'bytes': gc.mem_free()
+    }
+
+
+def get_file_list(path):
+    files = []
+
+    for f in os.listdir(path):
+        fp = '/'.join([path, f])
+        print('* checking', f)
+        s = os.stat(fp)
+        if s[0] & S_IFMT == S_IFDIR:
+            files.append((f, s[6], True, get_file_list(fp)))
+        else:
+            files.append((f, s[6], False, None))
+
+    return files
+
+
+@app.route('/file')
+def list_files(req):
+    print('* request to list files')
+    return get_file_list('/')
+
+
+@app.route('/file/(.*)')
+def get_file(req, path):
     print('* request to get {}'.format(path))
     try:
         with open(path) as fd:
-            return fd.read()
+            return chunked_reader(fd)
     except OSError:
         raise HTTPError(404)
 
 
-@app.route('/files/(.*)', method='PUT')
-def put_file(req, match):
-    path = match.group(1)
-    print('* request to put {}'.format(path))
+@app.route('/file/(.*)', methods=['DELETE'])
+def del_file(req, path):
+    print('* request to delete {}'.format(path))
+    try:
+        os.remove(path)
+    except OSError as err:
+        if err.args[0] == errno.ENOENT:
+            raise HTTPError(404)
+        else:
+            raise HTTPError(500)
 
-    parts = path.split(b'/')
+
+@app.route('/file/(.*)', methods=['POST'])
+def rename_file(req, path):
+    newpath = req.text
+    print('* request to rename {} -> {}'.format(path, newpath))
+    try:
+        os.rename(path, newpath)
+    except OSError as err:
+        if err.args[0] == errno.ENOENT:
+            raise HTTPError(404)
+        else:
+            raise HTTPError(500)
+
+
+@app.route('/file/(.*)', methods=['PUT'])
+def put_file(req, path):
+    print('* request to put {}'.format(path))
+    parts = path.split('/')
+
     for i in range(len(parts) - 1):
-        partial = b'/'.join(parts[:i + 1])
+        partial = '/'.join(parts[:i + 1])
         print('* create directory {}'.format(partial))
         try:
             os.mkdir(partial)
         except OSError:
             pass
 
-    tmppath = path + b'_otatmp'
-    with open(tmppath, 'w') as fd:
-        fd.write(req.content)
-
-    os.rename(tmppath, path)
-
-
-@app.route('/files/(.*)', method='DELETE')
-def del_file(req, match):
-    path = match.group(1)
-    print('* request to delete {}'.format(path))
-
-    try:
-        try:
-            os.remove(path)
-        except OSError as err:
-            if err.args[0] == errno.EISDIR:
-                os.rmdir(path)
-            else:
-                raise
-    except OSError as err:
-        if err.args[0] == errno.ENOENT:
-            raise HTTPError(404)
-        else:
-            raise
+    with open(path, 'w') as fd:
+        for chunk in req.iter_content():
+            fd.write(chunk)
 
 
 @app.route('/reset')
-def reset(req, match):
+def reset(req):
     open('_otareset', 'w').close()
-    board.reset()
-
-
-@app.route('/id')
-def get_id(req, match):
-    return board.id()
+    req.close()
+    machine.reset()
 
 
 def serve():
